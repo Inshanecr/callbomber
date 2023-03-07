1a5551d3dcaf512911d68747a27d553a82be06d0
VCH <92458450+Inshanecr@users.noreply.github.com>
2023-03-07 14:27:14 +0330

    Delete getfile.py

--- getfile.py
+++
@@ -1,9 +0,0 @@
-import os 
-a = os.listdir("pornhub")
-for i in a:
-
-    b = os.listdir(f"pornhub/{i}")
-    for c in b:
-       gg = (f"pornhub/{i}/{c}")
-       with open("i.py","a+") as file:
-            file.write(str(gg)+"\n")
